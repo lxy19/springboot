8f22b0fadf98053c5188f3aa92fba05d80217b76
Xiangye Li <xil954@g.harvard.edu>
2021-12-28 11:58:17 -0500

    first commit

---
+++ src/main/java/com/pluralsight/controllers/SpeakerController.java
@@ -0,0 +1,40 @@
+package com.pluralsight.controllers;
+
+import com.pluralsight.models.Speaker;
+import com.pluralsight.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/speakers")
+public class SpeakerController {
+    @Autowired
+    private SpeakerRepository speakerRepository;
+
+    @GetMapping
+    public List<Speaker> list() {
+        return speakerRepository.findAll();
+    }
+
+    @GetMapping
+    @RequestMapping("{id}")
+    public Speaker get(@PathVariable Long id) {
+        return speakerRepository.getById(id);
+    }
+
+    @RequestMapping(value="{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id) {
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value="{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker) {
+        //add a validation that all attributes are passed in, otherwise return a 400 bad payload
+        Speaker existingSpeaker = speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker, existingSpeaker, "speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
+    }
+}
---
+++ src/main/java/com/pluralsight/repositories/SessionRepository.java
@@ -0,0 +1,8 @@
+package com.pluralsight.repositories;
+
+import com.pluralsight.models.Session;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SessionRepository extends JpaRepository<Session, Long> {
+
+}
